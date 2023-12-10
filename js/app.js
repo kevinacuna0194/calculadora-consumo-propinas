14f8a4632403c9d52805c8bb41c1e00902157c85
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-12-10 11:22:01 -0300

    Mostrando más información de la API

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ let cliente = {
     pedido: []
 };
 
+const categorias = {
+    1: 'Comida',
+    2: 'Bebidas',
+    3: 'Postres'
+}
+
 const btnGuardarCiente = document.querySelector('#guardar-cliente');
 btnGuardarCiente.addEventListener('click', guardarCliente);
 
@@ -95,13 +101,23 @@ function mostrarPlatillos(platillos) {
     platillos.forEach(platillo => {
 
         const row = document.createElement('DIV');
-        row.classList.add('row');
+        row.classList.add('row', 'py-3', 'border-top');
 
         const nombre = document.createElement('DIV');
         nombre.classList.add('col-md-4');
         nombre.textContent = platillo.nombre;
 
+        const precio = document.createElement('DIV');
+        precio.classList.add('col-md-3', 'fw-bold');
+        precio.textContent = `$${platillo.precio}`;
+
+        const categoria = document.createElement('DIV');
+        categoria.classList.add('col-md-3', 'fw-bold');
+        categoria.textContent = categorias[platillo.categoria];
+
         row.appendChild(nombre);
+        row.appendChild(precio);
+        row.appendChild(categoria);
         contenido.appendChild(row);
     });
 }
